4f388db86fab3da5c55319599eaf62ffef1adf03
vincentinttsh <vincentinttsh@gmail.com>
2021-05-02 13:20:35 +0800

    app test change

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,24 @@
-import React from 'react'
+import React from 'react';
 import { shallow } from 'enzyme/build'
 import App from './App'
+import Page404 from 'src/views/pages/page404/Page404.js'
+import Register from 'src/views/user/register/Register.js'
+
+jest.mock('react-redux', () => ({
+  useSelector: jest.fn().mockImplementation(selector => selector()),
+})); // Closing bracket was missing so updated
 
 it('mounts App without crashing', () => {
   const wrapper = shallow(<App/>)
   wrapper.unmount()
 })
 
+it('mounts Page404 without crashing', () => {
+  const wrapper = shallow(<Page404 />);
+  wrapper.unmount()
+})
+
+it('mounts Register without crashing', () => {
+  const wrapper = shallow(<Register />);
+  wrapper.unmount()
+})
